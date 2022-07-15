5510a8a60f384754b2415c86d1b9912ed36d4344
Pablo Madrigal <pablomadrigal.1995@gmail.com>
2022-07-14 21:00:29 -0600

    Proyecto 2: Crear clase Job#16
    Proyecto 2: Crear clase Job#16

---
+++ 2/serial/enchanted_forest/src/include/Job.hpp
@@ -0,0 +1,25 @@
+// Copyright 2022 Ariel Arevalo Alvarado <ariel.arevalo@ucr.ac.cr>.
+// Copyright 2022 Pablo Madrigal Ramírez <pablo.madrigalramirez@ucr.ac.cr>.
+
+#pragma once
+
+#include <cstddef>
+#include <utility>
+#include <string>
+#include <vector>
+
+class Job {
+public :
+
+    Job(size_t id, std::string inputPath, std::string outputPath, std::vector<std::string> mapFiles)
+            : id(id), inputPath(inputPath), outputPath(outputPath), mapFiles(mapFiles) {}
+
+    const size_t id;
+
+    const std::string inputPath;
+
+    const std::string outputPath;
+
+    const std::vector<std::string> mapFiles;
+};
+
