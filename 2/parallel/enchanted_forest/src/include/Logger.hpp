1e16863e6879536bc68dcc2531e972717d62045e
Ariel Arévalo Alvarado <arielarevaloalvarado@gmail.com>
2022-07-22 13:39:17 -0600

    Revert "Proyecto 2: Se terminó la implementación inicial de MPI para paralelizar la lectura de archivos."
    This reverts commit 7778499c3527bf05db51d997db5e8ff7c82aa3ff.

--- 2/parallel/enchanted_forest/src/include/Logger.hpp
+++ 2/parallel/enchanted_forest/src/include/Logger.hpp
@@ -2,12 +2,10 @@
 
 #pragma once
 
-#include <mpi.h>
 #include <chrono>
 #include <cstdint>
 #include <iostream>
 #include <string>
-#include <filesystem>
 
 /**
  * @brief Logs info and error messages, and unwinds nested exceptions.
@@ -47,15 +45,7 @@ public:
 	 */
 	static void setStart();
 
-	static void initialize();
-	static void finalize();
 private:
-	static constexpr char LOG_DIR[8]{"./logs/"};
-
-	static constexpr char LOG_PATH[12]{ "./logs/log_" };
-
-	static constexpr char TXT[5]{".txt"};
-
 	/**
 	 * @brief Determines the time it takes to process each method or action.
  	 * @details Method that records the processing time
@@ -70,8 +60,6 @@ private:
 	 */
 	static void print_exception(const std::exception& e, int level = 0);
 
-	static MPI_File file;
-
 	static std::chrono::high_resolution_clock::time_point start;
 };
 
@@ -79,24 +67,16 @@ std::chrono::high_resolution_clock::time_point Logger::start{
 		std::chrono::high_resolution_clock::now()
 };
 
-MPI_File Logger::file;
-
 void Logger::info(const std::string& message)
 {
-	std::string output{};
-	output = "[" + std::to_string(duration()) + " ms]" + "[INFO]: "
-			  + message + "\n";
-	MPI_File_write(file, output.data(), static_cast<int>(output.size()),
-			MPI_CHAR, MPI_STATUS_IGNORE);
+	std::cout << "[" << duration() << " ms]" << "[INFO]: "
+			  << message << std::endl;
 }
 
 void Logger::error(const std::string& message)
 {
-	std::string output{};
-	output = "[" + std::to_string(duration()) + " ms]" + "[ERROR]: "
-			  + message + "\n";
-	MPI_File_write(file, output.data(), static_cast<int>(output.size()),
-			MPI_CHAR, MPI_STATUS_IGNORE);
+	std::cerr << "[" << duration() << " ms]" << "[ERROR]: "
+			  << message << std::endl;
 }
 
 void Logger::error(const std::string& message, const std::exception& e)
@@ -113,10 +93,7 @@ u_int64_t Logger::duration()
 
 void Logger::print_exception(const std::exception& e, int level)
 {
-	std::string output{};
-	output = "Caused by: " + std::string(e.what()) + "\n";
-	MPI_File_write(file, output.data(), static_cast<int>(output.size()),
-			MPI_CHAR, MPI_STATUS_IGNORE);
+	std::cerr << "Caused by: " << e.what() << std::endl;
 	try
 	{
 		std::rethrow_if_nested(e);
@@ -140,27 +117,7 @@ std::string Logger::deduce_exception_what(const std::exception& e)
 	return e.what();
 }
 
-void Logger::initialize()
-{
-	int rank{-1};
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-	if(rank == 0) {
-		std::filesystem::remove_all(LOG_DIR);
-		std::filesystem::create_directory(LOG_DIR);
-	}
-	MPI_Barrier(MPI_COMM_WORLD);
-	std::string filename{LOG_PATH + std::to_string(rank) + TXT};
-	MPI_File_open(MPI_COMM_WORLD, filename.data(), MPI_MODE_CREATE | MPI_MODE_WRONLY,
-			MPI_INFO_NULL, &file);
-}
-
 void Logger::setStart()
 {
 	start = std::chrono::high_resolution_clock::now();
-}
-
-void Logger::finalize()
-{
-	MPI_File_close(&file);
 }
